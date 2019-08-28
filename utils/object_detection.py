c951fac83ceb71b1b3cac3e75aeda9bccba5436a
Grant Perkins <24625614+GrantPerkins@users.noreply.github.com>
2019-08-28 10:44:15 -0800

    Implement networktables

--- utils/object_detection.py
+++ utils/object_detection.py
@@ -5,6 +5,7 @@ from edgetpu.detection.engine import DetectionEngine
 from PIL import Image
 from PIL import ImageDraw
 import cscore
+import networktables import NetworkTablesInstance
 import numpy as np
 from time import time
 
@@ -18,13 +19,25 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--model', help='Path of the detection model.', required=True)
+    parser.add_argument('--team', help="Your FIRST team number", type=int, default=190)
     args = parser.parse_args()
 
     # Initialize engine.
     engine = DetectionEngine(args.model)
     labels = {0: "hatch",
               1: "cargo"}
-
+    
+    ntinst = NetworkTablesInstance.getDefault()
+    ntinst.startClientTeam(args.team)
+    # integer, number of detected boxes at this curent moment
+    nb_boxes_entry = ntinst.getTable("ML").getEntry("nb_boxes")
+    # double array, list of boxes in the following format:
+    # [topleftx1, toplefty1, bottomrightx1, bottomrighty1, topleftx2, toplefty2, ... ]
+    # there are four numbers per box.
+    boxes_entry = ntinst.getTable("ML").getEntry("boxes")
+    # string array, list of class names of each box
+    boxes_names_entry = ntinst.getTable("ML").getEntry("boxes_names")
+    
     cs = cscore.CameraServer.getInstance()
     camera = cs.startAutomaticCapture()
     camera.setResolution(WIDTH, HEIGHT)
@@ -42,24 +55,32 @@ def main():
 
         # Run inference.
         ans = engine.DetectWithImage(frame, threshold=0.05, keep_aspect_ratio=True, relative_coord=False, top_k=10)
-
+        
+        nb_boxes_entry.setNumber(len(ans))
+        boxes = []
+        names = []
         # Display result.
         if ans:
             for obj in ans:
                 print('-----------------------------------------')
                 if labels:
                     print(labels[obj.label_id])
+                    names.append(labels[obj.label_id])
                 print('score = ', obj.score)
                 box = obj.bounding_box.flatten().tolist()
+                boxes.extend(box)
                 x1, y1, x2, y2 = box
                 print(abs(x1 - x2))
                 print('box = ', box)
                 # Draw a rectangle.
                 draw.rectangle(box, outline='green')
                 output.putFrame(np.array(frame))
+            
         else:
             print('No object detected!')
             output.putFrame(img)
+        boxes_entry.setDoubleArray(boxes)
+        boxes_names_entry.setStringArray(names)
         print("FPS:", 1 / (time() - start))
 
         start = time()
