71aaee626bbdfdef32c82de1c2b9073beebf56ea
Grant Perkins <24625614+GrantPerkins@users.noreply.github.com>
2019-09-11 08:48:35 -0800

    Update hyper.py

--- container/coral/hyper.py
+++ container/coral/hyper.py
@@ -7,4 +7,4 @@ Gets the number of training steps from SageMaker
 """
 if __name__ == "__main__":
     with open("/opt/ml/input/config/hyperparameters.json", 'r') as f:
-        print(json.load(f)["train-steps"])
+        print(json.load(f)["epochs"])
