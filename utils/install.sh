5de4637ec035e521f85626d11ef3a54581e885ad
Grant Perkins <24625614+GrantPerkins@users.noreply.github.com>
2020-01-02 01:50:24 -0500

    Remove old files (#10)

--- utils/install.sh
+++
@@ -1,7 +0,0 @@
-echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
-
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
-sudo apt-get update
-
-sudo apt-get install -y libedgetpu1-std python3-edgetpu
