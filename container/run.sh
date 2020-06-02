14a3f9778d679a1219df6819cf7742f9d1479608
GrantPerkins <gcperk20@gmail.com>
2020-06-02 18:38:55 -0400

    Mount to empty directory

--- container/run.sh
+++ container/run.sh
@@ -2,5 +2,5 @@ DIR=${PWD}
 echo ${DIR}
 docker run --name wpi \
        --rm -it --privileged -p 6006:6006 \
-       --mount type=bind,src=${DIR},dst=/tensorflow/models/research/learn \
+       --mount type=bind,src=${DIR},dst=/opt/ml/model \
        wpi-cpu-local
