e1eb8bb5f718cc5d45d44f7e1c74ef25e230f51e
GrantPerkins <gcperk20@gmail.com>
2020-06-02 16:41:21 -0400

    Local training works

--- container/push_and_build.sh
+++ container/push_and_build.sh
@@ -27,6 +27,6 @@ aws ecr get-login-password | docker login --username AWS --password-stdin ${full
 # with the full name.
 
 docker build  -t ${algorithm_name} .
-# docker tag ${algorithm_name} ${fullname}
+docker tag ${algorithm_name} ${fullname}
 
-# docker push ${fullname}
+docker push ${fullname}
