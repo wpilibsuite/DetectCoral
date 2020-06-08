e899286262270ab8095542e7b183e4c94e42ce32
GrantPerkins <gcperk20@gmail.com>
2020-06-08 12:39:39 -0400

    Testing works.

--- testing/run.sh
+++ testing/run.sh
@@ -1,6 +1,6 @@
 DIR=${PWD}
 echo ${DIR}
-docker run --name wpi \
-       --rm -it --privileged -p 6006:6006 \
+docker run --name wpi2 \
+       --rm -it -p 6006:6006 \
        --mount type=bind,src=${DIR},dst=/opt/ml/model \
-       wpi-cpu-local
+       wpi-test-local bash
--- training/run.sh
+++ training/run.sh
@@ -1,6 +1,6 @@
 DIR=${PWD}
 echo ${DIR}
 docker run --name wpi \
-       --rm -it --privileged -p 6006:6006 \
+       --rm -it -p 80:8080 \
        --mount type=bind,src=${DIR},dst=/opt/ml/model \
        wpi-cpu-local
