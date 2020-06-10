1e2dbd2c82d765de6b51c0bb5f29fb3c0acb1905
Nestor <nlzx432@gmail.com>
2020-06-10 15:09:44 -0400

    new prepare checkpoint script and rewritten tar_to_record

--- training/coral/tar_to_record.py
+++ training/coral/tar_to_record.py
@@ -1,26 +1,26 @@
-def convert():
-    import sys, os, glob, shutil, tarfile
-    import json_to_csv, generate_tfrecord, parse_meta
+import sys, os, glob, shutil, tarfile
+import json_to_csv, generate_tfrecord, parse_meta
 
+if __name__ == "__main__":
     outputpath = '/opt/ml/input/data/training'
-    shutil.rmtree(outputpath+'/out', ignore_errors=True)
-    os.mkdir(outputpath+'/out')
-    os.mkdir(outputpath+'/tmp')
+    shutil.rmtree(outputpath + '/out', ignore_errors=True)
+    os.mkdir(outputpath + '/out')
+    os.mkdir(outputpath + '/tmp')
 
-    tar_file = glob.glob(outputpath+"/*.tar")[0]
+    tar_file = glob.glob(outputpath + "/*.tar")[0]
     print(tar_file)
-    shutil.copy(tar_file, outputpath+'/data.tar')
+    shutil.copy(tar_file, outputpath + '/data.tar')
 
-    tar_file = tarfile.open(outputpath+'/data.tar')
-    tar_file.extractall(outputpath+'/out/') # specify which folder to extract to
+    tar_file = tarfile.open(outputpath + '/data.tar')
+    tar_file.extractall(outputpath + '/out/')  # specify which folder to extract to
     tar_file.close()
 
     json_to_csv.main()
 
-    generate_tfrecord.main(outputpath+'/tmp/train.csv',outputpath+'/train.record')
+    generate_tfrecord.main(outputpath + '/tmp/train.csv', outputpath + '/train.record')
 
-    generate_tfrecord.main(outputpath+'/tmp/eval.csv',outputpath+'/eval.record')
+    generate_tfrecord.main(outputpath + '/tmp/eval.csv', outputpath + '/eval.record')
 
-    parse_meta.main(outputpath+'/map.pbtxt')
+    parse_meta.main(outputpath + '/map.pbtxt')
 
     print(".\nRecords generated")
