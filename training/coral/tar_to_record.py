1c9f15115a5149bf34f5b8fbb0aabf73f1c96d5f
NLZ432 <NLZ432@gmail.com>
2020-06-12 00:16:28 -0700

    tar_to_record.py now allows users to submit their own datasets by placing them into the mount. If they choose not to, tar_to_record will instead use the default dataset that is installed in the dockerfile.  The tar compression of the model files has also been fixed, and no longer uses a subprocess. A decision still must be made about where the uncompressed copies of the files should go, as they still remain in the mount. Also, some minor edits to simplify path names.

--- training/coral/tar_to_record.py
+++ training/coral/tar_to_record.py
@@ -2,16 +2,26 @@ import sys, os, glob, shutil, tarfile
 import json_to_csv, generate_tfrecord, parse_meta
 from os.path import join
 
-if __name__ == "__main__":
-    outputpath = '/opt/ml/input/data/training'
+reserve_dataset_path = '/opt/ml/input/data/training/full_data.tar'
+
+def main(datasetpath, outputpath):
+    
     shutil.rmtree(join(outputpath,'out'), ignore_errors=True)
     shutil.rmtree(join(outputpath,'tmp'), ignore_errors=True)
     os.mkdir(join(outputpath,'out'))
     os.mkdir(join(outputpath,'tmp'))
 
-    tar_file = glob.glob(join(outputpath,"*.tar"))[0]
-    shutil.copy(tar_file, join(outputpath,'data.tar'))
+    tar_file = reserve_dataset_path
+    datasetlist = glob.glob(join(datasetpath,'*.tar'))
+    if len(datasetlist) != 0:
+        tar_file = datasetlist[0]
     print(tar_file)
+
+    try:
+        shutil.copy(tar_file, join(outputpath,'data.tar'))
+    except:
+        print('unable to retrieve the dataset tar file.')
+        sys.exit(1)
 
     with tarfile.open(join(outputpath,'data.tar')) as tar_file:
         tar_file.extractall(join(outputpath,'out'))
@@ -25,3 +35,13 @@ if __name__ == "__main__":
     parse_meta.main(join(outputpath,'map.pbtxt'))
 
     print(".\nRecords generated")
+
+if __name__ == "__main__":
+    
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--datasetdir', help='This is the path to the directory where you will put you desired dataset before training. It should be alone.', type=str, required=True)
+    parser.add_argument('-o', '--outputdir', help='This is the path to the directory that the records will go. It should only be /opt/ml/input/data/training right now.', type=str, required=True)
+    args = parser.parse_args()
+    
+    main(args.datasetdir,args.outputdir)
--- training/coral/train.py
+++ training/coral/train.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python2.7
 
-import os, shutil, fileinput, tarfile
-import labels, sed, modularized_model_main
-
-import subprocess  # << the goal is to get rid of the need for this
+import os, shutil, fileinput, tarfile, subprocess
+import labels, sed, modularized_model_main, tar_to_record
+from os.path import join
 
 def main():
 
-    classes = labels.get()
-
-    sed.replace_words('NUM_CLASSES', str(classes), "pipeline.config")
-
     TRAIN_STEPS = 1000
     BATCH_SIZE = 32
+    outputdir = '/opt/ml/model/'
 
+    tar_to_record.main(outputdir, '/opt/ml/input/data/training/')
+
+    classes = labels.get()
+    sed.replace_words('NUM_CLASSES', str(classes), "pipeline.config")
     sed.replace_words('BATCH_SIZE_PARAM', str(BATCH_SIZE), "pipeline.config")
 
     shutil.copy('pipeline.config','learn/ckpt/pipeline.config')
-
     shutil.rmtree('learn/train', ignore_errors=True)
     os.mkdir('learn/train')
 
@@ -34,15 +33,15 @@ def main():
     print('\nconverting checkpoint to tflite')
     subprocess.check_call("./convert_checkpoint_to_edgetpu_tflite.sh --checkpoint_num %s" % (str(TRAIN_STEPS)),
                           shell=True)
-    shutil.copy('learn/models/output_tflite_graph.tflite', '/opt/ml/model/unoptimized.tflite')
+    shutil.copy('learn/models/output_tflite_graph.tflite', join(outputdir,'unoptimized.tflite'))
 
     print('\ncompiling model for edge TPU')
-    subprocess.check_call("edgetpu_compiler ./learn/models/output_tflite_graph.tflite -o /opt/ml/model/", shell=True)
+    subprocess.check_call("edgetpu_compiler ./learn/models/output_tflite_graph.tflite -o %s"%(outputdir), shell=True)
 
-    with tarfile.open('/opt/ml/model/model.tar.gz','w:gz') as model:
-        model.add('/opt/ml/model/output_tflite_graph_edgetpu.tflite',arcname='model.tflite')
+    with tarfile.open(join(outputdir,'model.tar.gz'),'w:gz') as model:
+        model.add(join(outputdir,'output_tflite_graph_edgetpu.tflite'),arcname='model.tflite')
         model.add('/opt/ml/input/data/training/map.pbtxt',arcname='map.pbtxt')
-        model.add('/opt/ml/model/unoptimized.tflite',arcname='unoptimized.tflite')
+        model.add(join(outputdir,'unoptimized.tflite'),arcname='unoptimized.tflite')
 
     print('All done.')
 
