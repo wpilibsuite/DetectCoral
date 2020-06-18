662e5d7c364251f17ac28f6e1c98a95c48285766
NLZ432 <NLZ432@gmail.com>
2020-06-17 23:37:36 -0700

    Adapted this tensorboard event record analysis script to work with our program, by providing the training progress and eval number to derive epoch number.

---
+++ training/coral/log_loader.py
@@ -0,0 +1,47 @@
+#adapted from program by Tom Runia
+
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+
+def plot_tensorflow_log(path):
+
+    plt.style.use('dark_background')
+
+    tf_size_guidance = {
+        'compressedHistograms': 10,
+        'images': 1,
+        'scalars': 1000,
+        'histograms': 1
+    }
+
+    accumulator = EventAccumulator(path, tf_size_guidance)
+    accumulator.Reload()
+
+    precision = accumulator.Scalars('DetectionBoxes_Precision/mAP')
+    recall = accumulator.Scalars('DetectionBoxes_Recall/AR@100')
+
+    steps = len(precision)
+
+    x = np.arange(steps)
+    y = np.zeros([steps, 2])
+
+    for i in xrange(steps):
+        y[i, 0] = precision[i][2]
+        y[i, 1] = recall[i][2]
+
+    plt.plot(x, y[:,0], color='C1', label='precision', linewidth=3)
+    plt.plot(x, y[:,1], color='C3',label='recall', linewidth=3)
+
+    plt.xlabel("Steps")
+
+    legend_elements = [Line2D([0], [0], color='C1', lw=4, label='precision'),
+                       Line2D([0], [0], color='C3', lw=4, label='recall')]
+
+    plt.legend(handles=legend_elements, loc='upper right', frameon=True)
+    plt.savefig('/tensorflow/models/research/gui/static/plott.png')
+
+    return steps
