a26a4ce3ba7008fe2aa130929157077ff1c1d10e
NLZ432 <NLZ432@gmail.com>
2020-06-17 23:33:43 -0700

    added seperate ports for tensorboard

--- training/run.sh
+++ training/run.sh
@@ -13,6 +13,6 @@ echo ${DIR}
 
 mkdir -p $DIR
 docker run --name wpi \
-       --rm -d -p 5000:5000 \
+       --rm -d -p 5000:5000 -p 6006:6006 \
        --mount type=bind,src=${DIR},dst=/opt/ml/model \
        wpi-cpu-local
