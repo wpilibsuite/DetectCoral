be29740cff67e14d1bce1fd6e5e9e10f79a1ece5
NLZ432 <NLZ432@gmail.com>
2020-06-21 16:01:06 -0700

    cleaned up variables and form submission a bit

--- training/coral/gui/train_gui.py
+++ training/coral/gui/train_gui.py
@@ -10,10 +10,11 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 #the available models/link maps etc should soon be in a container-wide json or something
 reserve_dataset_path = '/opt/ml/input/data/training/full_data.tar'
+ckptpath = 'learn/ckpt/'
 mountpath = '/opt/ml/model/'
 model_output = join(mountpath,'finished-models')
-trainjob = None
-exportjob = None
+trainjob = {'process' : None, 'tensorboard' : None}
+exportjob = {'process' : None}
 tag_list = []
 current_epoch = 0
 selected_data = 'none'
@@ -25,55 +26,68 @@ for directory in ['checkpoints','datasets','finished-models']:
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
 def home():
+    global trainjob, exportjob, current_epoch
 
     datasets = glob.glob(join(mountpath,'datasets/*.tar'))
     datasets.append(reserve_dataset_path)
 
     ckpts = os.listdir(join(mountpath,'checkpoints'))
-    ckpts += [dir for dir in os.listdir('learn/ckpt/') if os.path.isdir('learn/ckpt/%s'%(dir))]
+    ckpts += [dir for dir in os.listdir(ckptpath) if os.path.isdir(join(ckptpath,dir))]
+
+    if request.method == 'POST':
+        
+        if trainjob['process']:
+            return redirect(url_for('train'))
+
+        with open('status.txt','w') as status:
+            status.write('starting the train job')
+
+        if trainjob['tensorboard'] is not None:
+            trainjob['tensorboard'].terminate()
+
+        trainjob = {
+            'process' : None,
+            'checkpoint' : request.form['pretrainedmodel'],
+            'dataset' : request.form['dataset'],
+            'epochs' : request.form['train_steps'],
+            'batch_size' : request.form['batch_size'],
+            'eval_period' : request.form['eval_period'],
+            'name' : request.form['name'],
+            'tensorboard' : None
+        }
+        
+        if trainjob['checkpoint'] not in os.listdir(ckptpath):
+            shutil.copytree(join(mountpath,'checkpoints',trainjob['checkpoint']), join(ckptpath,trainjob['checkpoint']))
+
+        if 'tboard' in request.form:
+            trainjob['tensorboard'] = subprocess.Popen(['tensorboard','--logdir','learn/train'])
+
+        current_epoch = 0
+        exportjob = {'process' : None}
+        trainjob['process'] = subprocess.Popen(['python','train.py',
+            '-ptm', trainjob['checkpoint'],
+            '-b', trainjob['batch_size'],
+            '-dsp', trainjob['dataset'],
+            '-ep', trainjob['eval_period'],
+            '-n', trainjob['name'],
+            '-s', trainjob['epochs']])
+
+        return redirect(url_for('train'))
 
     return render_template('prepare.html',pretrainedmodels=ckpts,datasets=datasets)
 
 @app.route("/training", methods=['GET', 'POST'])
 def train():
-    global trainjob, epochs, eval_period, current_epoch, output_name, current_epoch, exportjob, tag_list, selected_data, pretrainedmodels
+    global trainjob, exportjob, current_epoch, tag_list, selected_data
 
     if request.method == "POST":
-        if 'name' in request.form and trainjob is None:
-
-            current_epoch = 0
-            exportjob = None
-            epochs = int(request.form['train_steps'])
-            output_name = request.form['name']
-            eval_period = int(request.form['eval_period'])
-            checkpoint = request.form['pretrainedmodel']
-
-            if checkpoint not in os.listdir('learn/ckpt/'):
-                if checkpoint not in os.listdir(join(mountpath,'checkpoints')):
-                    return redirect(url_for('home'))
-                shutil.copytree(join(mountpath,'checkpoints',checkpoint), 'learn/ckpt/%s'%(checkpoint))
-
-            with open('status.txt','w') as status:
-                status.write('starting the train job')
-
-            if 'tboard' in request.form:
-                subprocess.Popen(['tensorboard','--logdir','learn/train'])
-
-            shutil.rmtree('learn/train', ignore_errors=True)
-            shutil.copyfile('gui/static/placeholder.png','gui/static/plott.png')
-
-            trainjob = subprocess.Popen(['python','train.py',
-                '-ptm', checkpoint,
-                '-b',request.form['batch_size'],
-                '-dsp',request.form['dataset'],
-                '-ep',request.form['eval_period'],
-                '-n',request.form['name'],
-                '-s',request.form['train_steps']])
-
-        if 'finish' in request.form and exportjob is None:
-            trainjob.terminate()
-            trainjob = None
-            exportjob = subprocess.Popen(['python','export.py','-s',str(current_epoch),'-n',output_name,'-o',model_output])
+        if 'finish' in request.form and exportjob['process'] is None:
+            trainjob['process'].terminate()
+            exportjob['process'] = subprocess.Popen(['python','export.py',
+                '-s',str(current_epoch),
+                '-n',trainjob['name'],
+                '-o',model_output])
+            trainjob['process'] = None
             with open('status.txt','w') as status:
                 status.write('stopping the train job, starting the export to the mounted directory.')
 
@@ -81,25 +95,22 @@ def train():
             selected_data = request.form['graphselect']
 
         if 'copy' in request.form:
-            copy_checkpoint(current_epoch, output_name)
+            copy_checkpoint(current_epoch, trainjob['name'])
 
-    if trainjob:
-        with open('status.txt', 'r') as status:
-            state = status.readlines()
+    with open('status.txt', 'r') as status:
+        state = status.readlines()
 
-        log = glob.glob('learn/train/eval_0/*')
-        if len(log) != 0:
-            log_loader.plot_tensorflow_log(log[0], selected_data)
-            tag_list = log_loader.retrieve_tags()
+    if trainjob['process'] is not None:
+
         ckpts = glob.glob('learn/train/model.ckpt-*.meta')
         if len(ckpts) != 0:
             current_epoch = max([int(re.split('learn/train/model.ckpt-|.meta',ckpt)[1]) for ckpt in ckpts])
 
-    elif exportjob:
-        with open('status.txt', 'r') as status:
-            state = status.readlines()
+        log = glob.glob('learn/train/eval_0/*')
+        log_loader.plot_tensorflow_log(log, selected_data)
+        tag_list = log_loader.retrieve_tags()
 
-    state.append('epoch %s out of %s'%(current_epoch, epochs))
+    state.append('epoch %s out of %s'%(current_epoch, int(trainjob['epochs'])))
     return render_template('training.html', content=state, data=tag_list, selected_data=selected_data)
 
 if __name__ == '__main__':
--- training/coral/train.py
+++ training/coral/train.py
@@ -8,17 +8,18 @@ ckpt_name_map={}
 ckpt_name_map["mobilenet_v1_ssd"]="ssd_mobilenet_v1_quantized_300x300_coco14_sync_2018_07_18"
 ckpt_name_map["mobilenet_v2_ssd"]="ssd_mobilenet_v2_quantized_300x300_coco_2019_01_03"
 
+outputdir = '/opt/ml/model/finished-models'
+trainpath = 'learn/train/'
+
 def logstatus(message):
     print(message)
     with open('status.txt','w') as status:
         status.write(message)
 
-def main(name, steps, batch, checkpoint, data_set_path, steps_per_eval=100):
+def main(name, TRAIN_STEPS, BATCH_SIZE, checkpoint, data_set_path, steps_per_eval=100):
 
-    TRAIN_STEPS = steps
-    BATCH_SIZE = batch
-    OUTPUT_NAME = '%s.tar.gz'%(name)
-    outputdir = '/opt/ml/model/finished-models'
+    shutil.rmtree(trainpath, ignore_errors=True)
+    os.mkdir(trainpath)
 
     logstatus('preparing the dataset')
     tar_to_record.main(data_set_path, '/opt/ml/input/data/training/')
@@ -28,44 +29,20 @@ def main(name, steps, batch, checkpoint, data_set_path, steps_per_eval=100):
         if '/tensorflow/models/research/learn/ckpt/%s/model.ckpt'%(checkpoint) not in config:
             sed.replace_words('/tensorflow/models/research/learn/ckpt/ssd_mobilenet_v2_quantized_300x300_coco_2019_01_03/model.ckpt',
                 '/tensorflow/models/research/learn/ckpt/%s/model.ckpt'%(checkpoint), "pipeline.config")
-
-    #must make function to switch to the pretrained models specific .config
+    
     classes = labels.get()
     sed.replace_words('NUM_CLASSES', str(classes), "pipeline.config")
     sed.replace_words('BATCH_SIZE_PARAM', str(BATCH_SIZE), "pipeline.config")
-
     shutil.copy('pipeline.config','learn/ckpt/pipeline.config')
-    shutil.rmtree('learn/train', ignore_errors=True)
-    os.mkdir('learn/train')
-
-    # here we should write the class tags of
-    # /opt/ml/input/data/training/Raw Data/meta.json to a labelmap file.
-    with open('labels.txt', 'w') as labelfile:
-        labelfile.writelines(['0 powercell'])
 
     logstatus('training the model')
     modularized_model_main.main(
         pipeline_config_path='learn/ckpt/pipeline.config',
-        model_dir='learn/train',
+        model_dir=trainpath,
         num_train_steps=TRAIN_STEPS,
         eval_period=steps_per_eval)
 
     export.main(TRAIN_STEPS, outputdir, name)
-    # logstatus('converting model to tflite')
-    # subprocess.check_call("./convert_checkpoint_to_edgetpu_tflite.sh --checkpoint_num %s" % (str(TRAIN_STEPS)),
-    #                       shell=True)
-    # shutil.copy('learn/models/output_tflite_graph.tflite', join(outputdir,'unoptimized.tflite'))
-
-    # logstatus('compiling model for edgetpu')
-    # subprocess.check_call("edgetpu_compiler ./learn/models/output_tflite_graph.tflite -o %s"%(outputdir), shell=True)
-
-    # logstatus('compressing model to tar')
-    # with tarfile.open(join(outputdir,OUTPUT_NAME),'w:gz') as model:
-    #     model.add(join(outputdir,'output_tflite_graph_edgetpu.tflite'),arcname='model.tflite')
-    #     model.add('/opt/ml/input/data/training/map.pbtxt',arcname='map.pbtxt')
-    #     model.add(join(outputdir,'unoptimized.tflite'),arcname='unoptimized.tflite')
-
-    # logstatus('Finished! Get your model.')
 
 if __name__ == "__main__":
     import argparse
