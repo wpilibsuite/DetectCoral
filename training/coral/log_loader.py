82381038335e16921c5b459b6bb1d6dc0bccea90
NLZ432 <NLZ432@gmail.com>
2020-06-21 01:11:00 -0700

    allows user to retrieve available tags, select tag to display

--- training/coral/log_loader.py
+++ training/coral/log_loader.py
@@ -7,8 +7,16 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+accumulator = False
 
-def plot_tensorflow_log(path):
+def retrieve_tags():
+    if accumulator:
+        return [i.encode('ascii') for i in accumulator.Tags()['scalars']]
+    else:
+        return []
+
+def plot_tensorflow_log(path, tag1):
+    global accumulator
 
     plt.style.use('dark_background')
 
@@ -23,28 +31,32 @@ def plot_tensorflow_log(path):
     accumulator.Reload()
 
     try:
-        precision = accumulator.Scalars('DetectionBoxes_Precision/mAP')
-        recall = accumulator.Scalars('DetectionBoxes_Recall/AR@100')
+        data1 = accumulator.Scalars(tag1)
+        # data2 = accumulator.Scalars(tag2)
     except:
         shutil.copyfile('gui/static/placeholder.png','gui/static/plott.png')
         return
 
-    steps = len(precision)
+    steps = len(data1)
 
     x = np.arange(steps)
     y = np.zeros([steps, 2])
 
     for i in xrange(steps):
-        y[i, 0] = precision[i][2]
-        y[i, 1] = recall[i][2]
+        y[i, 0] = data1[i][2]
+        # y[i, 1] = data2[i][2]
 
-    plt.plot(x, y[:,0], color='C1', label='precision', linewidth=3)
-    plt.plot(x, y[:,1], color='C3',label='recall', linewidth=3)
+    plt.plot(x, y[:,0], color='C1', label=tag1, linewidth=3)
+    # plt.plot(x, y[:,1], color='C3', label=tag2, linewidth=3)
 
     plt.xlabel("Steps")
 
-    legend_elements = [Line2D([0], [0], color='C1', lw=4, label='precision'),
-                       Line2D([0], [0], color='C3', lw=4, label='recall')]
+    legend_elements = [Line2D([0], [0], color='C1', lw=4, label=tag1)]
+                    #    Line2D([0], [0], color='C3', lw=4, label=tag2)]
 
     plt.legend(handles=legend_elements, loc='upper right', frameon=True)
     plt.savefig('/tensorflow/models/research/gui/static/plott.png')
+
+    plt.clf()
+    plt.cla()
+    plt.close()
