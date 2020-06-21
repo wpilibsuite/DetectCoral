7157c2106136fad5ee5cde5737650468ed412d00
NLZ432 <NLZ432@gmail.com>
2020-06-21 15:59:22 -0700

    now handles data retrieval directly from glob

--- training/coral/log_loader.py
+++ training/coral/log_loader.py
@@ -15,7 +15,7 @@ def retrieve_tags():
     else:
         return []
 
-def plot_tensorflow_log(path, tag1):
+def plot_tensorflow_log(log, tag1):
     global accumulator
 
     plt.style.use('dark_background')
@@ -26,37 +26,33 @@ def plot_tensorflow_log(path, tag1):
         'scalars': 1000,
         'histograms': 1
     }
+    
+    if len(log) != 0:
+        accumulator = EventAccumulator(log[0], tf_size_guidance)
+        accumulator.Reload()
+        if tag1 in retrieve_tags():
+            data1 = accumulator.Scalars(tag1)
+                
+            steps = len(data1)
+            x = np.arange(steps)
+            y = np.zeros([steps, 2])
 
-    accumulator = EventAccumulator(path, tf_size_guidance)
-    accumulator.Reload()
+            for i in xrange(steps):
+                y[i, 0] = data1[i][2]
+                # y[i, 1] = data2[i][2]
+            plt.plot(x, y[:,0], color='C1', label=tag1, linewidth=3)
 
-    try:
-        data1 = accumulator.Scalars(tag1)
-        # data2 = accumulator.Scalars(tag2)
-    except:
-        shutil.copyfile('gui/static/placeholder.png','gui/static/plott.png')
-        return
+            plt.xlabel("Steps")
+            legend_elements = [Line2D([0], [0], color='C1', lw=4, label=tag1)]
+            plt.legend(handles=legend_elements, loc='upper right', frameon=True)
 
-    steps = len(data1)
+            plt.savefig('/tensorflow/models/research/gui/static/plott.png')
 
-    x = np.arange(steps)
-    y = np.zeros([steps, 2])
+            plt.clf()
+            plt.cla()
+            plt.close()
+            return
+            
+    shutil.copyfile('gui/static/placeholder.png','gui/static/plott.png')
 
-    for i in xrange(steps):
-        y[i, 0] = data1[i][2]
-        # y[i, 1] = data2[i][2]
 
-    plt.plot(x, y[:,0], color='C1', label=tag1, linewidth=3)
-    # plt.plot(x, y[:,1], color='C3', label=tag2, linewidth=3)
-
-    plt.xlabel("Steps")
-
-    legend_elements = [Line2D([0], [0], color='C1', lw=4, label=tag1)]
-                    #    Line2D([0], [0], color='C3', lw=4, label=tag2)]
-
-    plt.legend(handles=legend_elements, loc='upper right', frameon=True)
-    plt.savefig('/tensorflow/models/research/gui/static/plott.png')
-
-    plt.clf()
-    plt.cla()
-    plt.close()
