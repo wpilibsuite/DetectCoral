9339f93db6f14d011c12de10de66d66d622a8891
GrantPerkins <gcperk20@gmail.com>
2020-07-17 17:50:01 -0400

    Write Precision json to mounted directory

--- mobilenet/training/scripts/modularized_model_main.py
+++ mobilenet/training/scripts/modularized_model_main.py
@@ -14,12 +14,9 @@ def main(
         model_dir,
         pipeline_config_path,
         num_train_steps,
-        eval_training_data=False,
         sample_1_of_n_eval_examples=1,
         sample_1_of_n_eval_on_train_examples=5,
         hparams_overrides=None,
-        checkpoint_dir=None,
-        run_once=False,
         eval_period=100):
     config = tf.estimator.RunConfig(model_dir=model_dir, save_checkpoints_steps=eval_period)
 
@@ -38,30 +35,13 @@ def main(
     predict_input_fn = train_and_eval_dict['predict_input_fn']
     train_steps = train_and_eval_dict['train_steps']
 
-    if checkpoint_dir:
-        if eval_training_data:
-            name = 'training_data'
-            input_fn = eval_on_train_input_fn
-        else:
-            name = 'validation_data'
-            # The first eval input will be evaluated.
-            input_fn = eval_input_fns[0]
-        if run_once:
-            estimator.evaluate(input_fn,
-                               steps=None,
-                               checkpoint_path=tf.train.latest_checkpoint(
-                                   checkpoint_dir))
-        else:
-            model_lib.continuous_eval(estimator, checkpoint_dir, input_fn,
-                                      train_steps, name)
-    else:
-        train_spec, eval_specs = model_lib.create_train_and_eval_specs(
-            train_input_fn,
-            eval_input_fns,
-            eval_on_train_input_fn,
-            predict_input_fn,
-            train_steps,
-            eval_on_train_data=False)
+    train_spec, eval_specs = model_lib.create_train_and_eval_specs(
+        train_input_fn,
+        eval_input_fns,
+        eval_on_train_input_fn,
+        predict_input_fn,
+        train_steps,
+        eval_on_train_data=False)
 
-        # Currently only a single Eval Spec is allowed.
-        tf.estimator.train_and_evaluate(estimator, train_spec, eval_specs[0])
+    # Currently only a single Eval Spec is allowed.
+    tf.estimator.train_and_evaluate(estimator, train_spec, eval_specs[0])
--- mobilenet/training/scripts/train.py
+++ mobilenet/training/scripts/train.py
@@ -7,12 +7,10 @@ import labels
 import modularized_model_main
 import parse_hyperparams
 import sed
-
-OUTPUT_PATH = '/opt/ml/model/finished-models'
-TRAIN_PATH = '/opt/ml/model/train'
-
+from log_parser import EvalJSONifier
 
 def main():
+    TRAIN_PATH = '/opt/ml/model/train'
     data = parse_hyperparams.parse("/opt/ml/model/hyperparameters.json")
     TRAIN_STEPS = data["epochs"]
     BATCH_SIZE = data["batch-size"]
@@ -31,16 +29,21 @@ def main():
     sed.replace_words('NUM_CLASSES', str(classes), "pipeline.config")
     sed.replace_words('BATCH_SIZE_PARAM', str(BATCH_SIZE), "pipeline.config")
 
-    subprocess.Popen(['tensorboard', '--logdir', 'learn/train'])
+    subprocess.Popen(['tensorboard', '--logdir', '/opt/ml/model/train'])
     shutil.copy('pipeline.config', '/opt/ml/model/pipeline.config')
 
+    json_thread = EvalJSONifier(TRAIN_STEPS)
+    json_thread.start()
+    print("Starting training.")
     modularized_model_main.main(
         pipeline_config_path='pipeline.config',
         model_dir=TRAIN_PATH,
         num_train_steps=TRAIN_STEPS,
         eval_period=EVAL_FREQ)
-
-
+    print("training done.")
+    json_thread.join()
+    print("Thread joined.")
 
 if __name__ == "__main__":
+    print("sanity check")
     main()
