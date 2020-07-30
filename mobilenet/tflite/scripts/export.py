bcf6840d9eb5d9cf6a6395930d1cd80c926e919c
NLZ432 <NLZ432@gmail.com>
2020-07-30 00:22:26 -0400

    add new export parameter that controls the export directory

--- mobilenet/tflite/scripts/export.py
+++ mobilenet/tflite/scripts/export.py
@@ -9,14 +9,15 @@ def main():
     model_dir = "/opt/ml/model/"
     unoptimized = "/tensorflow/models/research/learn/models/output_tflite_graph.tflite"
     second_export = "output_tflite_graph_edgetpu.tflite"
-    data = parse_hyperparams.parse(model_dir + "hyperparameters.json")
+    data = parse_hyperparams.parse(model_dir + "exportparameters.json")
 
     epoch = data["epochs"]
     output_name = data["name"]
+    export_dir = join(model_dir, data["export-dir"])
     subprocess.check_call("./convert_checkpoint_to_edgetpu_tflite.sh --checkpoint_num %s" % epoch, shell=True)
     subprocess.check_call("edgetpu_compiler %s -o %s" % (unoptimized, model_dir), shell=True)
 
-    with tarfile.open(join(model_dir, output_name + ".tar.gz"), 'w:gz') as model:
+    with tarfile.open(join(export_dir, output_name + ".tar.gz"), 'w:gz') as model:
         model.add(join(model_dir, second_export), arcname="model.tflite")
         model.add(model_dir + "map.pbtxt", arcname="map.pbtxt")
         model.add(unoptimized, arcname="unoptimized.tflite")
